9b2ff493cef1846fec2a5d2adcc0f298a52c38ad
zerofknideawhatimdoing <72335237+zerofknideawhatimdoing@users.noreply.github.com>
2022-04-01 21:52:34 +1100

    Update install-pyrit-kali-rolling-2021-1.sh

--- install-pyrit-kali-rolling-2021-1.sh
+++ install-pyrit-kali-rolling-2021-1.sh
@@ -3,10 +3,10 @@
 # Install dependencies
 sudo apt-get update
 sudo apt-get install -y \
-	python-dev \
+	python2-dev \
 	libssl-dev \
 	libpcap-dev \
-	python-scapy
+	python3-scapy
 
 # Clone Repo
 git clone https://github.com/JPaulMora/Pyrit.git --depth=1
@@ -18,7 +18,7 @@ sed -i "s/COMPILE_AESNI/COMPILE_AESNIX/" Pyrit/cpyrit/_cpyrit_cpu.c
 cd Pyrit
 python2.7 setup.py clean
 python2.7 setup.py build
-sudo python setup.py install
+sudo python2.7 setup.py install
 cd ..
 
 # Clean Up
